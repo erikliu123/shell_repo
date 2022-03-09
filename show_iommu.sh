5363653a03fd75a79ed182f3d4ab835fc91b991e
erikliu123 <1171045702@qq.com>
2022-03-09 12:11:56 +0800

    show_iommu.sh 查看系统IOMMU群组的情况 cuda_Makefile 用g++或者nvcc编译.cu文件的方法

---
+++ show_iommu.sh
@@ -0,0 +1,15 @@
+for iommu_group in $(find /sys/kernel/iommu_groups/ -maxdepth 1 -mindepth 1 -type d);
+do	
+        #if [[ $iommu_group != 40  ]]; then
+	#	continue;
+	#fi	
+	echo "IOMMU group $(basename "$iommu_group")"; 
+	for device in $(\ls -1 "$iommu_group"/devices/); 
+	do 
+		if [[ -e "$iommu_group"/devices/"$device"/reset ]]; then
+		       	echo -n "[RESET]"; 
+		fi; 
+		echo -n $'\t';lspci -nns "$device"; 
+	done; 
+done
+
